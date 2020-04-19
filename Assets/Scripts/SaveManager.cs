591bcc4ca4b6f479b3f59257073466be00670dec
AdamWon <ehdgus5500@gmail.com>
2020-04-19 14:32:21 +0900

    WrapUp

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -10,47 +10,91 @@ public class SaveManager : MonoBehaviour
 
     public void Save(char room, int level, int book = 0)
     {
+        // string path = GetPathFromSaveFile();
+        // using (FileStream stream = File.Open(path, FileMode.Create)) 
+        // {
+        //     Data data = new Data(room, level, book);
+        //     BinaryFormatter formatter = new BinaryFormatter();
+        //     formatter.Serialize(stream, data);
+        // }
+
+        Data data = new Data(room, level, book);
+        string JsonData = JsonUtility.ToJson(data);
+
         string path = GetPathFromSaveFile();
-        using (FileStream stream = File.Open(path, FileMode.Create)) 
+        using (FileStream stream = File.Open(path, FileMode.Create))
         {
-            Data data = new Data(room, level, book);
-            BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(stream, data);
+
+            byte[] byteData = Encoding.UTF8.GetBytes(JsonData);
+
+            stream.Write(byteData, 0, byteData.Length);
+
+            stream.Close();
+
+
+            Debug.Log(path + "// save completed");
         }
-        
         
     }
 
     public Data Load()
     {
-        string path = GetPathFromSaveFile();        
-        using (FileStream stream = File.Open(path, FileMode.Open)) 
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            Data data = (Data)formatter.Deserialize(stream);
+        // string path = GetPathFromSaveFile();        
+        // using (FileStream stream = File.Open(path, FileMode.Open)) 
+        // {
+        //     BinaryFormatter formatter = new BinaryFormatter();
+        //     Data data = (Data)formatter.Deserialize(stream);
 
-            return data;
-        }    
+        //     return data;
+        // }    
+
+
+        string path = GetPathFromSaveFile();
+        using (FileStream stream = File.Open(path, FileMode.Open))
+        {
+
+            byte[] byteData = new byte[stream.Length];
+
+            stream.Read(byteData, 0, byteData.Length);
+
+            stream.Close();
+
+            string JsonData = Encoding.UTF8.GetString(byteData);
+
+            Debug.Log(path + "// load completed");
+
+            return JsonUtility.FromJson<Data>(JsonData);
+
+        }        
 
     }
 
     private string GetPathFromSaveFile()
     {
-        return Path.Combine(Application.persistentDataPath, "save.sav");
+        // return Path.Combine(Application.persistentDataPath, "save.sav");
+        return Path.Combine(Application.persistentDataPath, "save.json");
     }
 
     public bool IsSaveFile()
     {
-        if(File.Exists(Path.Combine(Application.persistentDataPath, "save.sav")))
+        // if(File.Exists(Path.Combine(Application.persistentDataPath, "save.sav")))
+        // {
+        //     return true;
+        // }
+        // else return false;
+
+        if(File.Exists(Path.Combine(Application.persistentDataPath, "save.json")))
         {
             return true;
         }
-        else return false;
+        else return false;        
     }
 
     public void DeleteSaveData()
     {
-        File.Delete(Path.Combine(Application.persistentDataPath, "save.sav"));
+        // File.Delete(Path.Combine(Application.persistentDataPath, "save.sav"));
+
+        File.Delete(Path.Combine(Application.persistentDataPath, "save.json"));  
     }
 
 
